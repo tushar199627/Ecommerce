fef5dbcc6b50f0c775d2dcea963892d6a51ae71c
Kranti Kumar <kkranti1998@gmail.com>
2022-07-27 15:50:43 +0530

    update

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const userController = require('../controllers/userController')
+const productController = require('../controllers/productController')
 const auth = require("../authentication/authentication")
- const router = express.Router()
+const router = express.Router()
 
  router.post("/register", userController.createUser)
  router.post("/login", userController.loginUser)
- router.post("/user/:userId/profile",auth.authentication, userController.getUserById)
+ router.get("/user/:userId/profile",auth.authentication, userController.getUserById)
  router.put("/user/:userId/profile", userController.updateUserProfile)
- router.get("/products/:productId", productController.getProductDetails)
+ router.get("/products/:productId", productController.getProductDetails )
  router.delete("/products/:productId", productController.deleteProduct)
  
  module.exports = router;
