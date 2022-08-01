12b69b0280cc754e7ede04a9f919bf66eca94127
Kranti Kumar <kkranti1998@gmail.com>
2022-08-01 12:03:31 +0530

    Update

--- src/models/cartModel.js
+++ src/models/cartModel.js
@@ -2,12 +2,28 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const cartSchema = new mongoose.Schema({ 
-  userId: {type: ObjectId,ref: 'User',required: true,unique:true,trim: true,},
+  userId: {
+    type: ObjectId,
+    ref: 'User',
+    required: true,
+    unique:true,
+    trim: true
+},
   items: [{
-    productId: {type: ObjectId,ref: 'Product',required: true,trim: true,},
-    quantity: {type: Number,required: true,trim: true,min: 1},
-    _id: false
-  }],
+    productId: {
+        type: ObjectId,
+        ref: 'Product',
+        required: true,
+        trim: true,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        trim: true,
+        min: 1
+    },
+
+}],
   totalPrice: {type: number,required: true,trim: true},
   totalItems: {type: number,required: true,trim: true},
 },{timestamps: true});
