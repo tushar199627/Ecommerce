ab1c3c053c429a019e60f64a649a6df45b902bfc
tushar199627 <tusharbarai6@gmail.com>
2022-08-04 13:14:01 +0530

    updated

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,67 +1,39 @@
-const express = require("express");
-const userController = require("../controllers/userController");
-const productController = require("../controllers/productController");
-const cartController = require("../controllers/cartController");
-const orderController = require("../controllers/orderController");
-const auth = require("../authentication/authentication");
-const router = express.Router();
+const express = require('express')
+const userController = require('../controllers/userController')
+const productController = require('../controllers/productController')
+const cartController = require('../controllers/cartController')
+const orderController = require('../controllers/orderController')
+const auth = require("../authentication/authentication")
+const router = express.Router()
 
 //*********************USER API***********************************************
 
-router.post("/register", userController.createUser);
-router.post("/login", userController.loginUser);
-router.get(
-  "/user/:userId/profile",
-  auth.authentication,
-  userController.getUserById
-);
-router.put(
-  "/user/:userId/profile",
-  auth.authentication,
-  userController.updateUserProfile
-);
+ router.post("/register", userController.createUser)
+ router.post("/login", userController.loginUser)
+ router.get("/user/:userId/profile",auth.authentication, userController.getUserById)
+ router.put("/user/:userId/profile",auth.authentication, userController.updateUserProfile)
 
-//**********************************PRODUCT API**************************
+ //**********************************PRODUCT API**************************
 
-router.post("/products", productController.createProduct);
-router.get("/products", productController.getProductByFilter);
-router.get("/products/:productId", productController.getProductById);
-router.put("/products/:productId", productController.updateProductDetails);
-router.delete("/products/:productId", productController.deleteProduct);
+ router.post("/products", productController.createProduct)
+ router.get("/products", productController.getProductByFilter )
+ router.get("/products/:productId", productController.getProductById)
+ router.put("/products/:productId", productController.updateProductDetails)
+ router.delete("/products/:productId", productController.deleteProduct)
 
-//**********************************CART API**************************
+ //**********************************CART API**************************
 
-router.post(
-  "/users/:userId/cart",
-  auth.authentication,
-  cartController.createCart
-);
-router.put(
-  "/users/:userId/cart",
-  auth.authentication,
-  cartController.updatedCart
-);
-router.get(
-  "/users/:userId/cart",
-  auth.authentication,
-  cartController.getCartDetails
-);
-router.delete(
-  "/users/:userId/cart",
-  auth.authentication,
-  cartController.deleteCart
-);
+ 
+ router.post("/users/:userId/cart",auth.authentication, cartController.createCart)
+ router.put("/users/:userId/cart",auth.authentication, cartController.updatedCart)
+ router.get("/users/:userId/cart",auth.authentication, cartController.getCartDetails)
+ router.delete("/users/:userId/cart",auth.authentication, cartController.deleteCart)
 
-//**********************************ORDER API**************************
-router.post(
-  "/users/:userId/orders",
-  auth.authentication,
-  orderController.createOrder
-);
-router.put(
-  "/users/:userId/orders",
-  auth.authentication,
-  orderController.updateOrder
-);
 
-module.exports = router;
+ //**********************************ORDER API**************************
+ router.post('/users/:userId/orders', auth.authentication, orderController.createOrder)
+ router.put('/users/:userId/orders', auth.authentication,  orderController.updateOrder)
+
+ 
+ 
+ module.exports = router;
